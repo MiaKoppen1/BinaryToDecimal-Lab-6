fc84a6c0bb614cb416a8db454fe9378434910fd0
mia <143561609+MiaKoppen1@users.noreply.github.com>
2023-12-10 20:21:13 -0800

    Update Main.cpp
    updated some things

--- Lab #6/Main.cpp
+++ Lab #6/Main.cpp
@@ -5,49 +5,40 @@ Due: December 3rd
 Lab #6
 Description: Write a C++ program that reads characters representing binary (base 2)
 numbers from a data file called BinaryIn.dat and translates them to decimal (base 10)
-numbers. The binary and decimal numbers should be output in two columns with
-appropriate headings.
-There is only one binary number per input line, but an arbitrary number of blanks can
-precede the number. The program must read the binary numbers one character at a
-time. As each character is read, the program multiplies the total decimal value by 2 and
-adds either a 1 or 0, depending on the input character. The program should check for
-bad data; if it encounters anything except a 1 or 0, it should output the message ìBad
-digit on input.î The maximum number of spaces that could exist between any two digits
-of a binary number in the input file will be one, in which case, the binary digit would be
-considered bad. The output for this program should be to the screen and not an output
-file. In order to read in one character at a time, you will need to use the cin.get
-command. When you have completed this lab, upload a copy of the .cpp file called
-Lab6.cpp and BinaryIn.dat to GitHub. Make sure to use classes and proper comments
-and coding technique
+numbers. The binary and decimal numbers should be output in two columns.The program multiplies the total decimal value by 2 and
+adds either a 1 or 0. If it encounters anything except a 1 or 0, it should output the message ‚ÄúBad
+digit on input.‚Äù Use the cin.get
+command. 
 */
+
 #include "BinToDec.h"
 #include <iostream>
-#include <string>
 #include <fstream>
-#include <cmath>
+#include <string>
 
 int main()
 	{
+	BinToDec binaryToDecimal; //object created / Created an instance of the BinToDec class
+	const std::string filename = "C:/Users/KOPPEN11/Desktop/C++/Lab #6/BinaryIn.dat"; // Define the filename for the input binary file
 
-	BinToDec binaryToDecimal; //object created 
+	std::ifstream inputFile(filename); // Open the input file
 
-	const std::string filename = "C:/Users/KOPPEN11/Desktop/C++/Lab #6/BinaryIn.dat"; //naming the input file & opening the input file
-	std::ifstream inputFile(filename);
-
-	//check if the file is opened, if not, outputs a message (in  red) saying there was an error opening the file.
+	//check if the file is opened, if not, outputs an error message in red if the file opening fails
 	if (!inputFile.is_open())
 		{
 		std::cout << "\133[1;31mThere Was An Error Opening The File :( \033[0m" << filename << std::endl;
 		}
-	else //if the file opens succesffuly, outputs a message (in green) saying the file was opened succesfully.
+	else // Outputs a success message in green if the file is opened successfully
 		{
 		std::cout << filename << "   " << "\033[32;1mWas Opened Successfully :)\033[0m" << std::endl;
 		std::cout << std::endl;
-		binaryToDecimal.processFile(inputFile);
-		binaryToDecimal.convBinToDec(filename);
-		binaryToDecimal.createColumn();
-
-		inputFile.close();
 		}
-	return 0;
+
+		// Process the binary file using the BinToDec class
+		binaryToDecimal.processFile(inputFile);
+		// Convert binary to decimal using the BinToDec class
+		binaryToDecimal.convBinToDec(filename);
+
+	return 0; // Return 0 to indicate successful execution
+
 	}
